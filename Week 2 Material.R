7c5be73334588390609fc625c2767599191f8247
Bea Ambia <beaambia@gmail.com>
2020-11-17 19:07:56 -0600

    Instrucciones de la segunda semana de trabajo

---
+++ Week 2 Material.R
@@ -0,0 +1,14 @@
+# & evalúa un vector completo && evalua sólo el primer elemento de un vector
+# | evalúa un vector completo || evalua sólo el primer elemento de un vector
+
+identical(7,7)
+xor() #Es un or exclusivo, si un argumento es FALSE, entonces la función devolverá TRUE, en cualquier otro caso regresará FALSE
+
+# sample() es una función que da una muestra aleatoria sin reemplazo
+
+# which() toma un vector lógico como argumento y regresa los índices de los que son verdaderos
+
+# any() regresa TRUE si uno o más de los elementos lógicos es verdadero
+# all() regresa TRUE si sus elementos lógicos son verdadero
+
+# difftime(Sys.time(), t1, units = 'days') 
